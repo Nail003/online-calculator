2019f3737f3ff130b3d45e75aa6ef5ef8a6ad9b7
Nail003 <danialzafarstudy003@gmail.com>
2023-08-19 22:06:46 +0500

    FTR add keyboard inputs for calculator buttons

--- js/javascript.js
+++ js/javascript.js
@@ -8,7 +8,7 @@ import {
     operatorButtonHandler,
 } from "./eventHandlers.js";
 
-// Assigning click events to all calculator buttons
+// Assigning click and keyboard events to all calculator buttons
 const numberButtons = document.querySelectorAll(".numberButton");
 const operatorButtons = document.querySelectorAll(".operatorButton");
 const clearButton = document.querySelector(".clearButton");
@@ -19,12 +19,25 @@ const answerButton = document.querySelector(".answerButton");
 
 numberButtons.forEach((button) => {
     button.addEventListener("click", numberButtonHandler);
+    addKeyboardEvent(button, numberButtonHandler);
 });
-operatorButtons.forEach((button) =>
-    button.addEventListener("click", operatorButtonHandler)
-);
+operatorButtons.forEach((button) => {
+    button.addEventListener("click", operatorButtonHandler);
+    addKeyboardEvent(button, operatorButtonHandler);
+});
 clearButton.addEventListener("click", clearButtonHandler);
+addKeyboardEvent({ innerText: "Delete" }, clearButtonHandler);
 assignButton.addEventListener("click", assignButtonHandler);
+addKeyboardEvent(assignButton, assignButtonHandler);
+addKeyboardEvent({ innerText: "Enter" }, assignButtonHandler);
 changeSignButton.addEventListener("click", changeSignButtonHandler);
 delButton.addEventListener("click", delButtonHandler);
+addKeyboardEvent({ innerText: "Backspace" }, delButtonHandler);
 answerButton.addEventListener("click", answerButtonHandler);
+
+function addKeyboardEvent(domElement, handlerFunction) {
+    document.addEventListener("keydown", (e) => {
+        if (e.key === domElement.innerText)
+            handlerFunction({ target: domElement });
+    });
+}
